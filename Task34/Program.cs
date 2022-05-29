accd91312a27ebf0b4edcc2bf1a9cd8eb8048f07
Ivan <ivan241578@gmail.com>
2022-05-29 20:07:55 +0300

    Task34

---
+++ Task34/Program.cs
@@ -0,0 +1,35 @@
+﻿Console.WriteLine("Введите размер массива: ");
+int size = Convert.ToInt32(Console.ReadLine());
+int[] array = new int[size];
+void FillArray(int[] collection)
+{
+    for (int i = 0; i < collection.Length; i++)
+    {
+        collection[i] = new Random().Next(100, 1000);
+    }
+}
+void PrintArray(int[] col)
+{
+    for (int i = 0; i < col.Length; i++)
+    {
+        Console.Write($"{col[i]} ");
+    }
+}
+int EvenNumber(int[] collect)
+{
+    int count = 0;
+    for (int i = 0; i < collect.Length; i++)
+    {
+        if (collect[i] % 2 == 0)
+        {
+            count = count + 1;
+        }
+    }
+    return count;
+}
+
+FillArray(array);
+PrintArray(array);
+int even = EvenNumber(array);
+Console.WriteLine();
+Console.WriteLine(even);
